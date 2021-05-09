1e67bef9faabd3a1dd628f75b50091f2d1f9dc2e
Thomas Wreiner <thomaswreiner@gmail.com>
2021-05-09 19:31:42 +0200

    Changed project to backend only

--- api.js
+++ api.js
@@ -1,31 +1,37 @@
 'use strict';
 // Settings and Confs
 const express = require('express');
-const request = require('request'); 
+ 
 const axios = require('axios').default;
+
+const cors = require('cors');
 const { AuthorizationCode } = require('simple-oauth2');
-const mustacheExpress = require('mustache-express');
+
 
 const app = express();
 
 // Serve static files in Express
 app.use(express.static('public'));
 
-app.engine('html', mustacheExpress());
-app.set('view engine', 'html');
-app.set('views', __dirname + '/views');
-
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(express.json());
+
+// Makes it CORS friendly, handles a preflight check.
+app.options('/search', cors());
 
 const credentials = require('./credentials'); //  File with Spotify API account details
+
+// A demo object of a spotify playlist, for testing.
+const plDemo = require('./pl-demo.js');
 
 // Callback URL, change depending on local dev or domain hosted.
 const callbackURL = 'http://localhost:3000/callback';
 
+const frontendURL = 'http://localhost:8080';
 // Auth
 const oauth2 = new AuthorizationCode(credentials);
 
@@ -64,12 +70,18 @@ app.get('/callback', async (req, res) => {
 
   app.locals.token = accessToken;
 
- 	return res.status(200).cookie('auth', accessToken, {httpOnly: true}).redirect('/');
+  // TODO: Make the redirect work properly, carry the token or whatever
+
+  // Sets a cookie with the name "auth" with the token
+  res.cookie('auth', accessToken, {httpOnly: true});
+  console.log('req.cookies.auth: ' + JSON.stringify(req.cookies.auth));
+
+ 	return res.status(200).redirect(frontendURL);
   
  	
  } catch (error) {
    console.log('Access Token Error', error.message);
-   return res.status(500.).render(index.html, {message: 'Cannot obtain access token to Spotify'});
+   return res.status(500.).send({message: 'Cannot obtain access token to Spotify'});
    
  	 
  }
@@ -78,23 +90,39 @@ app.get('/callback', async (req, res) => {
 });
 
 
-app.post('/search', (req, res) => {
+app.post('/search', cors(), (req, res) => {
+
+  
+  let origin = req.headers.origin;
+  
+  console.log("req.headers.origin: " + origin);
 
   let token = req.cookies.auth;
+  console.log("token in /search with json.stringify:");
+  console.log(JSON.stringify(token));
+
   let tokenobj = app.locals.token;
+  console.log('tokenobj in search: ');
+  console.log(JSON.stringify(tokenobj));
 
   if (!tokenobj) {
-    return res.redirect('/');
+    console.log("Token false or null, sending noauth");
+    return res.send({noauth: 'Not authenticated. Try <a href="http://localhost:3000/auth">logging in</a>'});
   }
   
   // If token expired at the top. No search before token is approved and not undefined.
   // If expired, make a token.refresh and make sure cookie auth is updated.
+  console.log("Logged in.");
+  
 	let playlistUrl = req.body.search;
+  
+  console.log("Playlist URL: " + playlistUrl);
   // Convert a regular playlistURL into the ID
 	let playlistID = playlistUrl.replace(/:/g, '/').replace(/.*playlist\//, '')
       	.replace(/\?.*/, '');
+  
 
-
+  console.log("playlistID: " + playlistID);
 	
   console.log('search: is the token expired?');
   console.log(tokenobj.expired());
@@ -103,19 +131,23 @@ app.post('/search', (req, res) => {
 
 	if (!tokenobj.expired()) { 
 
-		// console.log(token);
+    console.log('access_token in search');
+		console.log(tokenobj.token.access_token);
+
+    console.log(Object.keys(tokenobj.token));
+
 		var searchOptions = {
           url: 'https://api.spotify.com/v1/playlists/' + playlistID + '/tracks',
-          headers: { 'Authorization': 'Bearer ' + token['access_token'] },
+          headers: { 'Authorization': 'Bearer ' + tokenobj.token.access_token},
           json: true
         };
 
         axios(searchOptions)
           .then(response => {
-            console.log(response);
-            res.render('index', {data: response.data});
+            console.log(response.data.items);
+            res.send(response.data.items);
           })
-          .catch(error => console.error('axios error'));
+          .catch(error => console.error('axios error:' + error));
 
         
 
@@ -125,7 +157,7 @@ app.post('/search', (req, res) => {
   // If token has expired. TODO: Refresh it.
 	else {
 
-		res.status(403).send('Not authenticated. Try <a href="/auth">logging in</a>');
+		res.status(403).send({noauth: 'Access token has expired. Try <a href="/auth">logging in</a>'});
 	}
 
 });
@@ -138,18 +170,18 @@ app.get('/clear_cookie', function(req, res){
 
 app.get('/', (req, res) => {
 
-  let tokenobj = app.locals.token;
-  console.log(tokenobj);
+  // let tokenobj = app.locals.token;
+  // console.log(tokenobj);
 
 	if (typeof tokenobj === 'undefined') {
 
 
 		// console.log("req.cookies.auth", req.cookies.auth);
 		
-    res.render('index', {message: 'Not authenticated. Try <a href="/auth">logging in</a>'})
+    res.send({message: 'Not authenticated. Try <a href="/auth">logging in</a>'})
 	}
 	else {
-		res.render('index', {message: 'Heeeellooooooo auth!'});
+		res.send({message: 'Heeeellooooooo auth!'});
 	}
 
 });
